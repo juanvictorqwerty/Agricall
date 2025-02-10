2b9a94c3ba5506d8ee431aae1f5da6e99be98cc8
Juan <juanvictorqwerty@gmail.com>
2025-02-10 21:08:57 +0100

    Started the Store page

--- agricall/views.py
+++ agricall/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from agricall.models import Products
+
 # Create your views here.
 #request handler part
 
 def welcome_user(request):
     return render(request,'index.html')
+
+def show_products(request):
+    products_list = Products.objects.all()
+    context = {
+    
+        'products': products_list
+
+    }
+    return render(request,'store.html',context)
